985f3a308f1c8e5a36eeba24c4446f77972d1c95
tianlinchun <260628258@qq.com>
2018-09-29 16:26:29 +0800

    修改细节问题

--- LifeCycleMaster/AppDelegate.swift
+++ LifeCycleMaster/AppDelegate.swift
@@ -17,6 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        // 解决导航条push时出现黑色
+        window?.backgroundColor = UIColor.white
         PrintLog()
         
         return true
--- LifeCycleMaster/ListTableViewController.swift
+++ LifeCycleMaster/ListTableViewController.swift
@@ -13,6 +13,7 @@ class ListTableViewController: UITableViewController {
     // MARK: - life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         PrintLog()
     }
     
